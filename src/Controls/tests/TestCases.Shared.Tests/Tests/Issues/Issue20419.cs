1ce05e0dee2f90ed8c96cd74522fa4a35a451322
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-04-16 19:23:02 +0530

    ICanvas.GetStringSize commit

---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20419.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue20419 : _IssuesUITest
+{
+	public Issue20419(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "Argument Exception raised when ICanvas.GetStringSize method called with default font";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void Issue20419ArgumentException()
+	{
+		App.WaitForElement("descriptionLabel");
+		VerifyScreenshot();
+	}
+}
