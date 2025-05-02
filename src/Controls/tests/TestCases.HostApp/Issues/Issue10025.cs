19450b55a7b3021f7861845a9070ee8577d1ba2d
SyedAbdulAzeemSF4852 <syedabdulazeem.a@syncfusion.com>
2025-05-02 12:57:26 +0530

    Test Case and fix for collection view selected item being set to null does nothing

---
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue10025.cs
@@ -0,0 +1,33 @@
+﻿namespace Controls.TestCases.HostApp.Issues;
+
+[Issue(IssueTracker.Github, 10025, "Assigning null to the SelectedItem of the CollectionView in the SelectionChanged event does not clear the selection as expected", PlatformAffected.UWP)]
+public class Issue10025 : ContentPage
+{
+	CollectionView collectionView;
+	public Issue10025()
+	{
+		collectionView = new CollectionView
+		{
+			SelectionMode = SelectionMode.Single,
+			ItemsSource = new List<string> { "Item1", "Item2", "Item3", "Item4", "Item5" },
+			ItemTemplate = new DataTemplate(() =>
+			{
+				Label label = new Label();
+
+				label.SetBinding(Label.TextProperty, ".");
+				label.SetBinding(Label.AutomationIdProperty, ".");
+
+				return label;
+			})
+		};
+
+		collectionView.SelectionChanged += SelectionChangedEvent;
+
+		Content = collectionView;
+	}
+
+	private void SelectionChangedEvent(object sender, SelectionChangedEventArgs e)
+	{
+		collectionView.SelectedItem = null;
+	}
+}
---
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue10025.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue10025 : _IssuesUITest
+{
+	public Issue10025(TestDevice device) : base(device)
+	{
+	}
+
+	public override string Issue => "Assigning null to the SelectedItem of the CollectionView in the SelectionChanged event does not clear the selection as expected";
+
+	[Test]
+	[Category(UITestCategories.CollectionView)]
+	public void VerifySelectedItemClearsOnNullAssignment()
+	{
+		App.Tap("Item2");
+		VerifyScreenshot();
+	}
+}
